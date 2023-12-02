1d73b04e5c8d52660d952ef1d4d2e768869eb7a9
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-12-02 13:41:43 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,19 @@
 import { getToken } from "next-auth/jwt";
-import { withAuth } from "next-auth/middleware";
-import { NextFetchEvent, NextResponse } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req: any, event: NextFetchEvent) {
-  const token = await getToken({ req });
-  const isAuthenticated = !!token;
+export async function middleware(req: NextRequest, _event: NextFetchEvent) {
+  const auth = req.nextUrl.clone();
+  auth.pathname = "/auth/login";
+  const afterAuth = req.nextUrl.clone();
+  afterAuth.pathname = "/dashboard";
 
-  if (req.nextUrl.pathname.startsWith("/auth") && isAuthenticated) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    const session = await getToken({ req: req as any });
+    if (!session) return NextResponse.redirect(auth);
   }
 
-  const authMiddleware = withAuth({
-    pages: {
-      signIn: `/auth/login`,
-    },
-  });
-
-  // @ts-expect-error
-  return authMiddleware(req, event);
+  if (req.nextUrl.pathname.startsWith("/auth")) {
+    const session = await getToken({ req: req as any });
+    if (session) return NextResponse.redirect(afterAuth);
+  }
 }
